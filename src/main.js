b1354665a07b5a8fb980fd1071883936328417ad
Letushev <o.m.letushev@gmail.com>
2021-03-06 18:40:45 +0200

    responsive banner

--- src/main.js
+++ src/main.js
@@ -3,26 +3,28 @@ import { enableScrolling, disableScrolling } from './js/prevent-scroll';
 import Logo from './js/animated-logo';
 
 window.addEventListener('DOMContentLoaded', () => {
-  disableScrolling();
+  if (window.innerWidth <= 848) {
+    init();
+  } else {
+    disableScrolling();
+    const photo = document.querySelector('.banner__photo');
+    photo.addEventListener('load', () => {
+      enableScrolling();
+      init();
+    });
+  }
+});
 
-  const photo = document.querySelector('.banner__photo');
+function init() {
+  const background = document.querySelector('.banner__background');
+  background.classList.add('banner__background--visible');
 
-  // after main photo is loaded:
-  // - enable scrolling
-  // - animate background
-  // - show all content with opacity transition
-  // - start animated-logo
-  photo.addEventListener('load', () => {
-    enableScrolling();
+  const wrapper = document.querySelector('.page-wrapper');
+  wrapper.classList.add('page-wrapper--visible');
     
-    const background = document.querySelector('.banner__background');
-    background.classList.add('banner__background--visible');
-
-    const wrapper = document.querySelector('.page-wrapper');
-    wrapper.classList.add('page-wrapper--visible');
-    
+  window.addEventListener('load', () => {
     const canvas = document.querySelector('.animated-logo__canvas');
     const logo = new Logo(canvas);
     logo.animate();
   });
-});
+}
