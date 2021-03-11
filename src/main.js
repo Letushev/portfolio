a2f0b7421c50974c5ca799d1f8117b9f38c44fae
Letushev <o.m.letushev@gmail.com>
2021-03-11 22:21:00 +0200

    simplify entry js code, decrease banner sizes, change greeting name style

--- src/main.js
+++ src/main.js
@@ -1,48 +1,14 @@
 import './styles/main.scss';
-import { enableScrolling, disableScrolling } from './js/prevent-scroll';
 import Logo from './js/animated-logo';
 
-let initialized = false;
-
-window.addEventListener('DOMContentLoaded', () => {
-  if (window.innerWidth <= 848) {
-    init();
-  } else {
-    disableScrolling();
-    const photo = document.querySelector('.banner__photo');
-
-    if (photo.complete) {
-      init();
-    } else {
-      photo.addEventListener('load', () => {
-        init();
-      });
-    }
-
-    setTimeout(() => {
-      if (!initialized) {
-        init();
-      }
-    }, 3000);
-  }
-});
-
-function init() {
-  initialized = true;
-  enableScrolling();
-
-  const background = document.querySelector('.banner__background');
-  background.classList.add('banner__background--visible');
-
-  const wrapper = document.querySelector('.page-wrapper');
-  wrapper.classList.add('page-wrapper--visible');
-
-  if (document.readyState === 'complete') {
-    initLogo();
-  } else {
-    window.addEventListener('load', initLogo);
-  }
+if (document.readyState === 'complete') {
+  initLogo();
+} else {
+  window.addEventListener('load', initLogo);
 }
+
+const background = document.querySelector('.banner__background');
+background.classList.add('banner__background--visible');
 
 function initLogo() {
   const canvas = document.querySelector('.animated-logo__canvas');
