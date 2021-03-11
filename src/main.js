c9c12765a2bb768a8283a42384eccb29f3352f8a
Letushev <o.m.letushev@gmail.com>
2021-03-11 22:26:59 +0200

    background wave transition without js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,7 @@
 import './styles/main.scss';
 import Logo from './js/animated-logo';
 
-if (document.readyState === 'complete') {
-  initLogo();
-} else {
-  window.addEventListener('load', initLogo);
-}
-
-const background = document.querySelector('.banner__background');
-background.classList.add('banner__background--visible');
+window.addEventListener('load', initLogo);
 
 function initLogo() {
   const canvas = document.querySelector('.animated-logo__canvas');
